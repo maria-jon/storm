6b09313309a3403a4b660570fbb48bb276196077
maria-jon <mariajonasson93@gmail.com>
2025-11-20 21:08:54 +0100

    change base URL in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://maria-jon.github.io',
-	base: '/storm',
+	site: 'https://maria-jon.github.io/storm',
+	//base: '/storm',
 	integrations: [mdx(), sitemap()],
 });
