320fb27f5814e866582abe531778f92769bfcc02
maria-jon <mariajonasson93@gmail.com>
2025-11-20 15:57:59 +0100

    add portfolio collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -16,4 +16,25 @@ const blog = defineCollection({
 		}),
 });
 
-export const collections = { blog };
+const portfolio = defineCollection({
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/portfolio', pattern: '**/*.md' }),
+	// Type-check frontmatter using a schema
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			// Transform string to Date object
+			date: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			cover: image(),
+			coverAlt: z.string(),
+			additionalImg: image().optional(),
+			link: z.string(),
+		}),
+});
+
+export const collections = { 
+	blog: blog, 
+	portfolio: portfolio,
+};
