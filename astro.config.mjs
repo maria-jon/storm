ccec55b3504e0133c54b921e594a8314c8c09a8c
Maria Jonasson <maria.jonasson@kan.se>
2026-02-23 10:52:35 +0100

    change base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://maria-jon.github.io',
-	base: '/storm',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 });
